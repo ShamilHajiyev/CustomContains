693874a646e2e9f1f02bc009f057d554e6003b95
ShamilHajiyev <shamilnh@code.edu.az>
2022-05-04 17:06:17 +0400

    initial commit

---
+++ CustomContains/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomContains
+{
+    static class Extensions
+    {
+        public static bool CustomCont(this string sentence, string word)
+        {
+            sentence = sentence.ToLower();
+            word = word.ToLower();
+            StringBuilder found = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < sentence.Length; j++)
+                {
+                    if (sentence[j] == word[i])
+                    {
+                        found.Append(sentence[j]);
+                        sentence = sentence.Remove(0, j + 1);
+                        break;
+                    }
+                    else if (i > 0 && found.Length > 0)
+                    {
+                        found.Remove(0, 1);
+                        i--;
+                    }
+                }
+            }
+            
+            if (word == found.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CustomContains/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomContains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence:");
+            string sentence = Console.ReadLine();
+            Console.WriteLine("\nEnter the word:");
+            string word = Console.ReadLine();
+
+            Console.WriteLine("\n" + sentence.CustomCont(word));
+            
+        }
+    }
+}
